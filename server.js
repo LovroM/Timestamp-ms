eff5983b36e894b97448630ba758a03e3f98bf44
Lovro <mekis.lovro@gmail.com>
2016-02-23 11:36:36 +0000

    got unix input part to work

--- server.js
+++ server.js
@@ -3,9 +3,25 @@ var app = express();
 var path = require("path");
 
 app.get('/', function(req, res) {
-    
     res.sendfile(path.join(__dirname, '/client', '/index.html'));
+});
 
+app.get("/:id", function(req, res) {
+    var id = String(req.params.id);
+    var responseObj = {"unix": null, "natural": null};
+    var monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+    
+    if (/[0-9]{10}/.test(id)) {
+        responseObj.unix = Number(id);
+        
+        var date = new Date(id * 1000);
+        responseObj.natural = monthNames[date.getMonth()] + " " + date.getDay() +", "+ date.getFullYear();
+    }//else if(/reg/.test(id)) {}
+    
+    
+    res.send(responseObj);
 });
 
 
